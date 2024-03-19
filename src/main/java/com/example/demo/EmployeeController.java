21a0b0fa23eb8bbaf4d5bde877589b5f4a6ceeb1
snavale2 <snavale@binghamton.edu>
2024-03-19 00:11:18 -0400

    POST method implemented- Adding Employee

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 // Creating the REST controller 
 @RestController
@@ -29,5 +35,41 @@ public class EmployeeController {
         return employeeManager 
             .getAllEmployees(); 
     }
+
+    @PostMapping( 
+        path = "/", 
+        consumes = "application/json", 
+        produces = "application/json") 
+  
+    public ResponseEntity<Object> addEmployee( 
+        @RequestBody Employee employee) 
+    { 
+  
+        // Creating an ID of an employee 
+        // from the number of employees 
+        Integer id 
+            = employeeManager 
+                  .getAllEmployees() 
+                  .getEmployeeList() 
+                  .size() 
+              + 1; 
+  
+        employee.setEmployee_id(id); 
+  
+        employeeManager 
+            .addEmployee(employee); 
+  
+        URI location 
+            = ServletUriComponentsBuilder 
+                  .fromCurrentRequest() 
+                  .path("/{id}") 
+                  .buildAndExpand( 
+                      employee.getEmployee_id()) 
+                  .toUri(); 
+  
+               return ResponseEntity 
+            .created(location) 
+            .build(); 
+    } 
 } 
 
